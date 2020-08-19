7d356e4a86a054d6556fdf95f7564da63f5a2c6e
Olzhas <o.arystanov@gmail.com>
2020-08-19 22:35:49 +0600

    Designed celery tasks

---
+++ aviata/tasks.py
@@ -0,0 +1,53 @@
+from celery.utils.log import get_task_logger
+
+from aviata.celery_app import app
+from aviata.exceptions import CheckInProgress
+from aviata.settings import Settings
+from aviata.utils import (
+    get_cheapest_flight,
+    get_flights_from_cache,
+    get_forward_dates,
+    verify_flight_not_changed,
+    write_flight_to_cache,
+)
+
+logger = get_task_logger(__name__)
+
+
+@app.task
+def update_flight(fly_from: str, fly_to: str, date_from: str):
+    logger.info(f"Requesting flights for {fly_from}-{fly_to} on {date_from}")
+    price, booking_token = get_cheapest_flight(fly_from, fly_to, date_from)
+    write_flight_to_cache(fly_from, fly_to, date_from, booking_token, price)
+
+
+@app.task
+def update_all_flights():
+    for day in get_forward_dates():
+        for row in Settings.FLIGHTS:
+            update_flight.delay(row[0], row[1], day.strftime("%d/%m/%Y"))
+
+
+@app.task(bind=True)
+def check_flight(
+    fly_from: str, fly_to: str, date_from: str, booking_token: str, **kwargs
+):
+    logger.info(f"Verifying flight {fly_from}-{fly_to} on {date_from}")
+    try:
+        valid = verify_flight_not_changed(booking_token)
+    except CheckInProgress as exc:
+        logger.info(
+            f"Check in progress. Scheduled retry in {Settings.FLIGHT_CHECK_DELAY}"
+        )
+        self.retry(exc=exc, countdown=Settings.FLIGHT_CHECK_DELAY)
+    if not valid:
+        logger.info(f"Flight info outdated. Scheduled update")
+        update_flight.delay(fly_from, fly_to, date_from)
+
+
+@app.task
+def check_all_flights():
+    flights = get_flights_from_cache()
+    for flight in flights:
+        params = flight.pop("price")
+        check_flight.delay(**params)
