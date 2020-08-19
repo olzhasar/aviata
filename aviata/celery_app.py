ee2024443e5b557973b0b10e9676f6c092eb19ea
Olzhas <o.arystanov@gmail.com>
2020-08-19 23:39:04 +0600

    Added celery beat schedule

--- aviata/celery_app.py
+++ aviata/celery_app.py
@@ -1,5 +1,17 @@
 from celery import Celery
+from celery.schedules import crontab
 
 app = Celery(
     "aviata", broker="amqp://aviata:aviata@rabbitmq:5672//", include="aviata.tasks"
 )
+
+app.conf.beat_schedule = {
+    "update-flights": {
+        "task": "aviata.tasks.update_all_flights",
+        "schedule": crontab(minute=0, hour=0),
+    },
+    "check-flights": {
+        "task": "aviata.tasks.check_all_flights",
+        "schedule": crontab(minute=0, hour="1-23"),
+    },
+}
